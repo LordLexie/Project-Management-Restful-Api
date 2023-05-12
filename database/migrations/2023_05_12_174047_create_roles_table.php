4b6fff74574a7fab73ab8a3682c6c75d9ee455e2
LordLexie <muigaifrancis@gmail.com>
2023-05-12 20:43:37 +0300

    Populated the migration file for roles table

--- database/migrations/2023_05_12_174047_create_roles_table.php
+++ database/migrations/2023_05_12_174047_create_roles_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', 100)->unique();
+            $table->string('code', 255)->nullable(false);
         });
     }
 
