c869a962255f82671022e36f2c16bc27211eea8e
LordLexie <muigaifrancis@gmail.com>
2023-05-12 20:50:50 +0300

    Created function store in Roles  controller

--- app/Http/Controllers/Api/V1/RolesController.php
+++ app/Http/Controllers/Api/V1/RolesController.php
@@ -5,7 +5,16 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\V1\StoreUserRequest;
+
 class RolesController extends Controller
 {
     //
+
+    public function store(StoreUserRequest $request)
+    {
+        return response()->json([
+            'message' => 'Hello World!',
+        ]);
+    }
 }
