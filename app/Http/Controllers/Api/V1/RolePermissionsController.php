73085797752699ed7a866bc9e8560f2375d5c166
LordLexie <muigaifrancis@gmail.com>
2023-05-22 12:34:50 +0300

    Imported model RolePermissions and its equivalent Request

--- app/Http/Controllers/Api/V1/RolePermissionsController.php
+++ app/Http/Controllers/Api/V1/RolePermissionsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\RolePermissions;
+use App\Http\Requests\V1\StoreRolePermissionsRequest;
+
 class RolePermissionsController extends Controller
 {
     //
