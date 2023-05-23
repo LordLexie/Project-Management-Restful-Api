34322492f54dca38793025e635f5be3185ff8992
LordLexie <muigaifrancis@gmail.com>
2023-05-23 16:41:41 +0300

    Created functionalities for managing Permissions

--- app/Http/Controllers/Api/V1/PermissionController.php
+++ app/Http/Controllers/Api/V1/PermissionController.php
@@ -5,7 +5,49 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Permission;
+use App\Http\Requests\v1\PermissionRequest;
+use App\Http\Resources\PermissionResource;
+use App\Http\Resources\PermissionCollection;
+
 class PermissionController extends Controller
 {
-    //
+    public function index()
+    {
+       return new PermissionCollection(Permission::all());    
+    }
+
+    public function show(Permission $permission)
+    {    
+        return PermissionResource::make($permission);    
+    } 
+    
+    public function store(PermissionRequest $request)
+    {
+        $data = $request->validated();
+        $permission = new Permission();
+        $permission->fill($data)->save();
+
+        return response()->json([
+            'data' => new PermissionResource($permission),
+            'message' => 'Permission created successfully!',            
+        ]);
+    }
+
+    public function update(PermissionRequest $request, Permission $permission)
+    {
+        $permission->update($request->validated());
+
+        return response()->json([
+            'data' => new PermissionResource($permission),
+            'message' => 'Permission updated successfully!',            
+        ]); 
+
+    }
+
+    public function destroy(Permission $permission)
+    {
+        $permission->delete();
+        return response()->noContent();
+    } 
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=>'v1','namespace'=>'App\Http\Controllers\Api\V1'], function(){
     Route::apiResource('users', 'UserController');
     Route::apiResource('roles', 'RoleController');
+    Route::apiResource('permissions', 'PermissionController');
 }); 
