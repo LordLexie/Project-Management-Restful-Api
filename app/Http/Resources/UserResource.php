cbe6e9cec23925b4895ff03f2db049db38b73c7d
LordLexie <muigaifrancis@gmail.com>
2023-05-23 15:27:07 +0300

    Created Resource for Role and User

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,19 +5,15 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UsersResource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
+     *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
     {
-       return [
-        'id' => $this->id,
-        'name' => $this->name,
-        'email'=> $this->email,
-        'role'=> is_null($this->role()->first()) ? 'Fucking' : $this->role()->first()->name,
-       ];
+        return parent::toArray($request);
     }
 }
