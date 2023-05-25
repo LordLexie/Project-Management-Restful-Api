9e7c300d4e89a8942816b5131deef7091686bb34
LordLexie <muigaifrancis@gmail.com>
2023-05-25 16:10:49 +0300

    Populated the migration file for the Project module

--- database/migrations/2023_05_25_130418_create_projects_table.php
+++ database/migrations/2023_05_25_130418_create_projects_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->uuid('folder_id')->unique();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('status')->default('active');
+            $table->string('location');
+            $table->integer('customer_id')->unsigned()->nullable(false);
         });
     }
 
