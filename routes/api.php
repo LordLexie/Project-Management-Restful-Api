aef863690850add8deb6d04c1d5cc8d09a5975aa
LordLexie <muigaifrancis@gmail.com>
2023-05-25 17:43:24 +0300

    Created resource route for Requisitions

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,8 @@ Route::group(['prefix'=>'v1','namespace'=>'App\Http\Controllers\Api\V1'], functi
     Route::apiResource('users', 'UserController')->middleware('auth:sanctum');
     Route::apiResource('roles', 'RoleController')->middleware('auth:sanctum');
     Route::apiResource('projects', 'ProjectController')->middleware('auth:sanctum');
-    Route::apiResource('customers', 'CustomerController')->middleware('auth:sanctum');     
+    Route::apiResource('customers', 'CustomerController')->middleware('auth:sanctum');
+    Route::apiResource('requisitions', 'RequisitionController')->middleware('auth:sanctum');   
     Route::apiResource('permissions', 'PermissionController')->middleware('auth:sanctum');    
     Route::apiResource('role_permission', 'RolePermissionController')->middleware('auth:sanctum');      
     Route::post('logout', 'LoginController@logout')->middleware('auth:sanctum');
