c0d443ad890c039f686f1552af1ff9de942c0630
LordLexie <muigaifrancis@gmail.com>
2023-05-25 16:17:06 +0300

    Update the ProjectRequest file

--- app/Http/Requests/V1/ProjectRequest.php
+++ app/Http/Requests/V1/ProjectRequest.php
@@ -11,7 +11,7 @@ class ProjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class ProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+            'location' => 'required|string',
         ];
     }
 }
