9e309204d3771da56c2f1fbc6cef0c38b85118e0
LordLexie <muigaifrancis@gmail.com>
2023-05-25 18:06:12 +0300

    Created Requisition Request

---
+++ app/Http/Requests/V1/RequisitionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RequisitionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'requisition_title' => 'required|string|max:255',
+            'requisition_description' => 'nullable|string',
+            'project_id' => 'required|integer|exists:projects,id',
+            'author_id' => 'required|integer|exists:users,id'
+        ];
+    }
+}
